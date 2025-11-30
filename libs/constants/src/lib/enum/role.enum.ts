ed85afdd118007efcb3db710c0d43b18dcb1f96a
manhtranduc <manhtranduc0202@gmail.com>
2025-11-30 15:53:40 +0700

    add role service

---
+++ libs/constants/src/lib/enum/role.enum.ts
@@ -0,0 +1,12 @@
+export enum ROLE {
+  ADMINISTRATOR = 'administrator',
+  ACCOUNTANT = 'accountant',
+}
+export enum PERMISSION {
+  INVOICE_CREATE = 'invoice.create',
+  INVOICE_GET_BY_ID = 'invoice.get_by_id',
+  INVOICE_GET_ALL = 'invoice.get_all',
+  INVOICE_UPDATE = 'invoice.update',
+  INVOICE_DELETE = 'invoice.delete',
+  INVOICE_SEND = 'invoice.send',
+}
