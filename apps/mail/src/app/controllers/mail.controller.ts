8f2df8866cf22af9de12f13e625b0fa56eb2c60c
manhtranduc <manhtranduc0202@gmail.com>
2026-01-02 18:33:22 +0700

    Gửi Email với Nodemailer qua Gmail SMTP

--- apps/mail/src/app/controllers/mail.controller.ts
+++ apps/mail/src/app/controllers/mail.controller.ts
@@ -6,11 +6,21 @@ import {
   Payload,
 } from '@nestjs/microservices';
 
+import { MailService } from '../modules/mail/services/mail.service';
 @Controller()
 export class MailController {
+  constructor(private readonly mailservice: MailService) {}
   // emit nên nó là EventPattern, còn send thì nó sẽ là messagepattern
   @EventPattern('invoice-send')
-  invoiceSendEvent(@Payload() Payload: any, @Ctx() context: KafkaContext) {
+  invoiceSendEvent(
+    @Payload() payload: { invoiceId: string; clientEmail: string },
+    @Ctx() context: KafkaContext
+  ) {
     // payload là cái gửi ở bên cái emit
+    this.mailservice.sendMail({
+      subject: 'Invoice',
+      to: payload.clientEmail,
+      text: `Invoice: ${payload.invoiceId}`,
+    });
   }
 }
