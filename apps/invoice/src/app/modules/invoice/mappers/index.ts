f573575dbc36e3592eb3a0e04e5a1d0626cb7b44
manhtranduc <manhtranduc0202@gmail.com>
2026-01-02 11:34:38 +0700

    tạo link thanh toán với stripe

--- apps/invoice/src/app/modules/invoice/mappers/index.ts
+++ apps/invoice/src/app/modules/invoice/mappers/index.ts
@@ -1,3 +1,4 @@
+import { CreateCheckoutSessionRequest } from '@common/interfaces/common/stripe.interface';
 import { CreateInvoiceTcpRequest } from '@common/interfaces/tcp/invoice';
 import { Invoice } from '@common/schemas/invoice.schema';
 
@@ -13,5 +14,18 @@ export const invoiceRequestMapping = (
         acc + item.unitPrice * item.quantity * (item.vatRate / 100),
       0
     ),
+  };
+};
+export const createCheckoutSessionMapping = (
+  invoice: Invoice
+): CreateCheckoutSessionRequest => {
+  return {
+    invoiceId: invoice._id.toString(),
+    clientEmail: invoice.client.email,
+    lineItems: invoice.items.map((item) => ({
+      name: item.name,
+      quantity: item.quantity,
+      price: item.total,
+    })),
   };
 };
